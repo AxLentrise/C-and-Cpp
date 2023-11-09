afffbeb60ab07b9389eff0dc250fb24cb758c2a3
Diogo Tiago Bertoldo <139913465+AxLentrise@users.noreply.github.com>
2023-11-09 15:12:42 -0300

    Update experimentos.cpp

--- revisao/estrutura-repetitiva/experimentos.cpp
+++ revisao/estrutura-repetitiva/experimentos.cpp
@@ -2,17 +2,17 @@
 #include <stdlib.h>
 
 /*Problema "experiencias" (adaptado de URI 1094)
-Maria acabou de iniciar seu curso de graduaÃ§Ã£o na faculdade de medicina e precisa de sua ajuda para 
-organizar os experimentos de um laboratÃ³rio o qual ela Ã© responsÃ¡vel. Ela quer saber no final do ano, 
-quantas cobaias foram utilizadas no laboratÃ³rio e o percentual de cada tipo de cobaia utilizada. Este 
-laboratÃ³rio em especial utiliza trÃªs tipos de cobaias: sapos, ratos e coelhos. Para obter estas 
-informaÃ§Ãµes, ela sabe exatamente o nÃºmero de experimentos que foram realizados, o tipo de cobaia 
-utilizada e a quantidade de cobaias utilizadas em cada experimento. FaÃ§a um programa que 
-leia um valor inteiro N que indica os vÃ¡rios casos de teste que vem a seguir. Cada caso de teste contÃ©m um 
+Maria acabou de iniciar seu curso de graduação na faculdade de medicina e precisa de sua ajuda para 
+organizar os experimentos de um laboratório o qual ela é responsável. Ela quer saber no final do ano, 
+quantas cobaias foram utilizadas no laboratório e o percentual de cada tipo de cobaia utilizada. Este 
+laboratório em especial utiliza três tipos de cobaias: sapos, ratos e coelhos. Para obter estas 
+informações, ela sabe exatamente o número de experimentos que foram realizados, o tipo de cobaia 
+utilizada e a quantidade de cobaias utilizadas em cada experimento. Faça um programa que 
+leia um valor inteiro N que indica os vários casos de teste que vem a seguir. Cada caso de teste contém um 
 inteiro que representa a quantidade de cobaias utilizadas e uma letra ('C', 'R' ou 'S'), indicando o tipo 
 de cobaia (R:Rato S:Sapo C:Coelho). Apresente o total de cobaias utilizadas, o total de cada tipo de 
-cobaia utilizada e o percentual de cada uma em relaÃ§Ã£o ao total de cobaias utilizadas, sendo que o 
-percentual deve ser apresentado com dois dÃ­gitos apÃ³s o ponto.*/
+cobaia utilizada e o percentual de cada uma em relação ao total de cobaias utilizadas, sendo que o 
+percentual deve ser apresentado com dois dígitos após o ponto.*/
 
 //-----------------------------------------------------------
 
@@ -35,7 +35,7 @@ int main(void){
         printf("Digite qual cobaia foi utilizada:\n\n[C] - Coelho;\n[S] - Sapo;\n[R] - Rato;\n\nR: ");
         fflush(stdin);
         scanf("%c", &cdgImput);
-        system("clear"); // cls for windows
+        system("cls"); // cls for windows
 
         switch(cdgImput){
             case 'C': qntdC += qntdExperimentos; break;
@@ -43,13 +43,23 @@ int main(void){
             case 'R': qntdR += qntdExperimentos; break;
         }
     }
+    
+    pctgmC = (float)(qntdC*100/qntdTotal);
+    pctgmR = (float)(qntdR*100/qntdTotal);
+    pctgmS = (float)(qntdS*100/qntdTotal);
 
     printf("Quantidade total de casos de testes realizados: %i\n", qntdCasos);
     printf("Quantidade total de experimentos realizados: %i\n\n", qntdTotal);
-    printf("Experimentos em Coelhos: %3.0i - %3.2f%\n", qntdC, (float)(100*qntdC/qntdTotal));
-    printf("Experimentos em Ratos:   %3.0i - %3.2f%\n", qntdR, (float)(100*qntdR/qntdTotal));
-    printf("Experimentos em Sapos:   %3.0i - %3.2f%\n", qntdS, (float)(100*qntdS/qntdTotal));
-
+    if(qntdC > 0){
+        printf("Experimentos em Coelhos: %3.0i - %.2f porcento\n", qntdC, pctgmC);
+    }
+    if(qntdR > 0){
+        printf("Experimentos em Ratos:   %3.0i - %.2f porcento\n", qntdR, pctgmR);
+    }
+    if(qntdS > 0){
+        printf("Experimentos em Sapos:   %3.0i - %.2f porcento\n", qntdS, pctgmS);
+    }
+    
     return 0;
 }
 
